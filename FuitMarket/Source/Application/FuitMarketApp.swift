c6f78c41f1fd04e878b953312bf6c5ec14dec3b6
limetree81 <limetree81@knu.ac.kr>
2024-05-16 23:12:58 +0900

    ch19

--- FuitMarket/Source/Application/FuitMarketApp.swift
+++ FuitMarket/Source/Application/FuitMarketApp.swift
@@ -10,10 +10,16 @@ import SwiftUI
 @main
 struct FuitMarketApp: App {
     var body: some Scene {
-        WindowGroup {
+        configurationAppearance()
+        return WindowGroup {
             Text("2019110074, 금경훈")
             //Home(store: Store()) //ch.12까지
-            Home().environmentObject(Store())
+            Home().accentColor(Color.primary).environmentObject(Store())
         }
+    }
+    
+    private func configurationAppearance() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(named: "peach")!]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(named: "peach")!]
     }
 }
